7e9abdb1ffa5fe4d181e99c5c23470cde1c7e0c0
Ricardo Sparapan <ricardo.ofice@hotmail.com>
2020-05-02 15:42:21 -0300

    consuming api git hub

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,62 +1,66 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import { FiChevronRight } from 'react-icons/fi';
 import { Title, Form, Repositories } from './styles';
 import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
+
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
 
 const Dashboard: React.FC = () => {
+    const [newEepo, setNewRepo] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    async function handleAddRepository(
+        event: FormEvent<HTMLFormElement>,
+    ): Promise<void> {
+        event.preventDefault();
+
+        const response = await api.get<Repository>(`repos/${newEepo}`);
+        const repository = response.data;
+
+        setRepositories([...repositories, repository]);
+        setNewRepo('');
+    }
+
     return (
         <>
             <img src={logoImg} alt="Github Explorer" />
             <Title>Explore repositórios no Github</Title>
 
-            <Form>
-                <input placeholder="Digite o nome do repositório" type="text" />
+            <Form onSubmit={handleAddRepository}>
+                <input
+                    placeholder="Digite o nome do repositório"
+                    type="text"
+                    value={newEepo}
+                    onChange={(e) => setNewRepo(e.target.value)}
+                />
                 <button type="submit">Pesquisar</button>
             </Form>
 
             <Repositories>
-                <a href="teste">
-                    <img
-                        src="https://avatars2.githubusercontent.com/u/34757240?s=460&u=aff2f7d37b1a87484bc7b8daca3c6bfdf8b475e3&v=4"
-                        alt="Ricardo Sparapan"
-                    />
+                {repositories.map((repository) => (
+                    <a href="teste" key={repository.full_name}>
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                        />
 
-                    <div>
-                        <strong>Ropo name</strong>
-                        <p>Desc fake</p>
-                    </div>
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
 
-                    <FiChevronRight size={20} />
-                </a>
-
-                <a href="teste">
-                    <img
-                        src="https://avatars2.githubusercontent.com/u/34757240?s=460&u=aff2f7d37b1a87484bc7b8daca3c6bfdf8b475e3&v=4"
-                        alt="Ricardo Sparapan"
-                    />
-
-                    <div>
-                        <strong>Ropo name</strong>
-                        <p>Desc fake</p>
-                    </div>
-
-                    <FiChevronRight size={20} />
-                </a>
-
-                <a href="teste">
-                    <img
-                        src="https://avatars2.githubusercontent.com/u/34757240?s=460&u=aff2f7d37b1a87484bc7b8daca3c6bfdf8b475e3&v=4"
-                        alt="Ricardo Sparapan"
-                    />
-
-                    <div>
-                        <strong>Ropo name</strong>
-                        <p>Desc fake</p>
-                    </div>
-
-                    <FiChevronRight size={20} />
-                </a>
+                        <FiChevronRight size={20} />
+                    </a>
+                ))}
             </Repositories>
         </>
     );
