f065f7891ce7dfd5daf0a958ede606bec5d39944
Ricardo Sparapan <ricardo.ofice@hotmail.com>
2020-05-02 12:51:31 -0300

    Styled dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,65 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositórios no Github</Title>;
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Title>Explore repositórios no Github</Title>
+
+            <Form>
+                <input placeholder="Digite o nome do repositório" type="text" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img
+                        src="https://avatars2.githubusercontent.com/u/34757240?s=460&u=aff2f7d37b1a87484bc7b8daca3c6bfdf8b475e3&v=4"
+                        alt="Ricardo Sparapan"
+                    />
+
+                    <div>
+                        <strong>Ropo name</strong>
+                        <p>Desc fake</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="teste">
+                    <img
+                        src="https://avatars2.githubusercontent.com/u/34757240?s=460&u=aff2f7d37b1a87484bc7b8daca3c6bfdf8b475e3&v=4"
+                        alt="Ricardo Sparapan"
+                    />
+
+                    <div>
+                        <strong>Ropo name</strong>
+                        <p>Desc fake</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="teste">
+                    <img
+                        src="https://avatars2.githubusercontent.com/u/34757240?s=460&u=aff2f7d37b1a87484bc7b8daca3c6bfdf8b475e3&v=4"
+                        alt="Ricardo Sparapan"
+                    />
+
+                    <div>
+                        <strong>Ropo name</strong>
+                        <p>Desc fake</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
